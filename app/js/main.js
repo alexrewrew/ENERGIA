e3fe3484f1e3f1805b4e1d950b638f506ee26a2e
maxkoval <maxkovalburn@gmail.com>
2017-09-08 20:46:14 +0300

    form (php folder not include)

--- app/js/main.js
+++ app/js/main.js
@@ -13417,8 +13417,83 @@ $(document).ready(function () {
     //     // document.getElementById("status").className = "disabled";
     // };
 
+    /**
+     * Form validation and sending
+     */
+    $("#contact-form").on("submit", function (e) {
+        e.preventDefault();
 
+        var form = $(this),
+            name = form.find("input[name='name']").val(),
+            email = form.find("input[name='email']").val(),
+            text = form.find("textarea").val(),
+            send = true,
+            r = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+        $(".error-p").html("").slideUp();
+
+        if (name === "") {
+            send = false;
+            form.find("input[name='name']").addClass("error").focus();
+            $(".error-p").html("Введіть ім'я").slideDown();
+        } else if (email === "") {
+            send = false;
+            form.find("input[name='email']").addClass("error").focus();
+            $(".error-p").html("Введіть e-mail").slideDown();
+        } else if (!r.test(email)) {
+            send = false;
+            form.find("input[name='email']").addClass("error").focus();
+            $(".error-p").html("Невірний формат вводу e-mail").slideDown();
+        } else if (text === "") {
+            send = false;
+            form.find("textarea").addClass("error").focus();
+            $(".error-p").html("Введіть текст повідомлення").slideDown();
+        }
+
+        if (send) {
+            var url = form.attr("action"),
+                data = form.serialize(),
+                method = form.attr("method");
+
+            $.ajax({
+                method: method,
+                url: url,
+                data: data,
+                success: function (data) {
+                    if (data === "") {
+                        form.trigger("reset");
+                        $('.form-visible').slideUp();
+                        $('.form-hidden').slideDown();
+                    } else {
+                        alert("Помилка! Спробуйте пізніше!");
+                    }
+                }
+            });
+        }
+    });
+
+    $(".okay-btn").on("click", function (e) {
+        e.preventDefault();
+
+        $("#contact-form").trigger("reset");
+        $('.form-visible').slideDown();
+        $('.form-hidden').slideUp();
+    });
+
+    $("#contact-form").find("input[name='name']").on("input", function () {
+        $(this).removeClass("error");
+        $(".error-p").html("").slideUp();
+    });
+
+    $("#contact-form").find("input[name='email']").on("input", function () {
+        $(this).removeClass("error");
+        $(".error-p").html("").slideUp();
+    });
+
+    $("#contact-form").find("textarea").on("input", function () {
+        $(this).removeClass("error");
+        $(".error-p").html("").slideUp();
+    });
 
     //responsive scripts
     $(window).on('load resize', function () {
---
+++ app/php/send.php
@@ -0,0 +1,40 @@
+<?php
+function mime_header_encode ($str, $data_charset = '', $send_charset = '')
+{
+    if ($data_charset != $send_charset) {
+        $str = iconv ($data_charset, $send_charset, $str);
+    }
+    return '=?' . $send_charset . '?B?' . base64_encode ($str) . '?=';
+}
+
+
+function send_mime_mail ($name_from, $email_from, $name_to, $email_to, $data_charset, $send_charset, $subject, $body)
+{
+    $to = mime_header_encode ($name_to, $data_charset, $send_charset) . ' <' . $email_to . '>';
+    $subject = mime_header_encode ($subject, $data_charset, $send_charset);
+    $from = mime_header_encode ($name_from, $data_charset, $send_charset) . ' <' . $email_from . '>';
+    if ($data_charset != $send_charset) {
+        $body = iconv ($data_charset, $send_charset, $body);
+    }
+    $headers = "From: $from\r\n";
+    $headers .= "Content-type: text/html; charset=$send_charset\r\n";
+    return mail ($to, $subject, $body, $headers);
+}
+
+if (!empty($_POST)) {
+    $arUsers = array(
+        array(
+            "NAME" => "ENERGIA",
+            "EMAIL" => "maxkovalburn@gmail.com" //change email here
+        )
+    );
+
+    $message = '<b>Ім\'я:</b>' . $_POST['name'] . '<br/>';
+    $message .= '<b>E-mail:</b>' . $_POST['email'] . '<br/><br/>';
+    $message .= '<b>Текст повідомлення:</b><br/>' . nl2br ($_POST['text']);
+
+    foreach ($arUsers as $Item) {
+        $r = send_mime_mail ($_POST['name'], $_POST['email'], $Item['NAME'], $Item['EMAIL'], 'utf-8', 'utf-8', "Повідомлення з сайту energia.ltd", $message);
+    }
+}
+?>
