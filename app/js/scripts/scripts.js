d606e52d51861e689f8f68b227a4309f36680130
alexrewrew <alexrew2012@gmail.com>
2017-09-20 17:05:51 +0300

    Pravki

--- app/js/scripts/scripts.js
+++ app/js/scripts/scripts.js
@@ -1,22 +1,6 @@
 $(document).ready(function () {
     "use strict";
 
-    // $("#modal").iziModal({
-    //     title:'Any random text',
-    // });
-    // $(document).on('click', '.trigger', function (event) {
-    //     event.preventDefault();
-    //     $('#modal').iziModal('open');
-    // });
-
-    // $(".inline").modaal();
-
-
-    // document.addEventListener('touchmove', function(e) {e.preventDefault();}, true);
-
-    // document.body.addEventListener('touchmove',function(event){event.preventDefault();},false);
-    // $(document).bind('touchmove', false);
-
 
     $('.slider').on('init reInit afterChange', function (event, slick, currentSlide) {
         var i = (currentSlide ? currentSlide : 0) + 1;
@@ -29,33 +13,6 @@ $(document).ready(function () {
         prevArrow: '<div class="slider-arrow arrow-left"><img src="img/slider/arrow-left.svg"/></div>'
     });
 
-
-    // $(".owl-carousel").owlCarousel();
-
-    // $(".animsition").animsition({
-    //     inClass: "fade-in",
-    //     outClass: "fade-out",
-    //     inDuration: 1500,
-    //     outDuration: 800,
-    //     linkElement: ".animsition-link",
-    //     // e.g. linkElement: "a:not([target="_blank"]):not([href^="#"])"
-    //     loading: true,
-    //     loadingParentElement: "body", //animsition wrapper element
-    //     loadingClass: "animsition-loading",
-    //     loadingInner: "", // e.g "<img src="loading.svg" />"
-    //     timeout: false,
-    //     timeoutCountdown: 5000,
-    //     onLoadEvent: true,
-    //     browser: ["animation-duration", "-webkit-animation-duration"],
-    //     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    //     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    //     overlay: false,
-    //     overlayClass: "animsition-overlay-slide",
-    //     overlayParentElement: "body",
-    //     transition: function (url) {
-    //         window.location.href = url;
-    //     }
-    // });
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -79,18 +36,6 @@ $(document).ready(function () {
             scrollTop: top
         }, 1500);
     });
-
-    // $('.nav-icon').click(function () {
-    //     disableScroll();
-    // })
-
-
-    // document.getElementById("disable").onclick = function() {
-    //     disableScroll();
-    //     // document.getElementById("status").innerHTML = "disabled";
-    //     // document.getElementById("status").className = "disabled";
-    // };
-
     /**
      * Form validation and sending
      */
